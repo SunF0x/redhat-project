f9346c8709bd6fbd1c97b73aaaeea54aa655755c
SunF0x <buzinakatya2000@mail.ru>
2024-01-09 23:58:32 +0300

    Fixed item count in localstorage

--- src/pages/Menu/Item.jsx
+++ src/pages/Menu/Item.jsx
@@ -11,7 +11,7 @@ const Item = (element) => {
       let dict = JSON.parse(localStorage.getItem('goods'));
       let flag = false;
       for (const [key, value] of Object.entries(dict)) {
-        if (key === element) {
+        if (key == element.toString()) {
           dict[key] = value + 1;
           flag = true;
           break;
