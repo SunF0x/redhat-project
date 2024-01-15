cf759e69712c3e07c44e40d2acaed3f165b75b45
SunF0x <buzinakatya2000@mail.ru>
2024-01-16 01:15:51 +0300

    Added report creation and reportId page

--- src/pages/Report/Report.jsx
+++ src/pages/Report/Report.jsx
@@ -1,6 +1,9 @@
 import './Report.css';
 import { Link, Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
 import { REACT_APP_API } from '../../config/config';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -12,25 +15,141 @@ import { getAccessToken } from '../../utils/accessToken';
 
 const Report = () => {
   const [app, setApp] = useState([]);
-  console.log(app);
+  const [create, setCreate] = useState(false);
+  const [file, setFile] = useState([]);
+
   if (!getAccessToken()) {
     return <Navigate to="/login" />;
   }
+
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append('Authorization', 'Bearer ' + localStorage.getItem('accessToken'));
+
   useEffect(() => {
     const asyncFn = async () => {
-      const myHeaders = new Headers();
-      myHeaders.append('Content-Type', 'application/json');
-      myHeaders.append('Authorization', 'Bearer ' + localStorage.getItem('accessToken'));
       const response = await fetch(`${REACT_APP_API}/report`, { headers: myHeaders }); //,{mode: 'no-cors'}
       const result = await response.json();
       setApp(result);
     };
     asyncFn();
   }, []);
+
+  const handleCreate = () => {
+    setCreate(!create);
+  };
+
+  const handleFile = (event) => {
+    // if (file && file.length > 0) {
+    //   setFile([...file, URL.createObjectURL(event.target.files[file.length])]);
+    // } else {URL.createObjectURL(
+    setFile(event.target.files[0]);
+    // }
+  };
+
+  console.log(file);
+
+  const addreport = () => {
+    const asyncReport = () => {
+      const formData = new FormData();
+      // file.map((el, i) => {
+      //   i == 0 ? formData.append(`photo`, el) : formData.append(`photo${i}`, el);
+      // });
+      formData.append(`photo`, file);
+      formData.append('data', document.getElementById('report').value);
+      formData.append('verdict', document.getElementById('verdict').value);
+      const myHeaders = new Headers();
+      // myHeaders.append('Content-Type', 'multipart/*');
+      myHeaders.append('Accept', 'application/json');
+      myHeaders.append('Authorization', 'Bearer ' + localStorage.getItem('accessToken'));
+      fetch(`${REACT_APP_API}/report`, {
+        method: 'POST',
+        headers: myHeaders,
+        body: formData
+      });
+    };
+    asyncReport();
+  };
+
   return (
     <div className="fon">
       <div className="pole1">
         <div className="title2">Мои отчеты</div>
+        <Button
+          type="submit"
+          variant="contained"
+          onClick={handleCreate}
+          style={{
+            position: 'absolute',
+            top: 70,
+            right: 40,
+            width: '180px',
+            height: '40px',
+            backgroundColor: `#90182E`,
+            fontFamily: 'El Messiri',
+            fontSize: 16
+          }}>
+          Создать отчет
+        </Button>
+        <Dialog onClose={() => setCreate(false)} open={create}>
+          <DialogContent className="bg-[rgb(245,229,231,0.9)]">
+            <div className="flex flex-col gap-4 mt-4">
+              <input
+                className="h-10 rounded-md pl-2"
+                id="report"
+                type="report"
+                label="report"
+                placeholder="Оставьте отзыв"
+              />
+              <input
+                className="h-10 rounded-md pl-2"
+                id="verdict"
+                type="verdict"
+                label="verdict"
+                placeholder="Вынесите вердикт"
+              />
+              <input
+                className="h-10 rounded-md pl-2 mt-2"
+                id="file"
+                type="file"
+                label="file"
+                multiple
+                onChange={handleFile}
+                accept="image/*,image/jpeg"
+                placeholder="Прикрепить изображение"></input>
+              <div className="flex flex-row gap-4">
+                <Button
+                  type="submit"
+                  variant="contained"
+                  onClick={addreport}
+                  style={{
+                    bottom: 0,
+                    right: 0,
+                    width: '180px',
+                    backgroundColor: `#90182E`,
+                    fontFamily: 'El Messiri',
+                    fontSize: 16
+                  }}>
+                  Создать отчет
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  onClick={() => setCreate(false)}
+                  style={{
+                    bottom: 0,
+                    right: 0,
+                    width: '150px',
+                    backgroundColor: `black`,
+                    fontFamily: 'El Messiri',
+                    fontSize: 16
+                  }}>
+                  Закрыть
+                </Button>
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
         <Table sx={{ minWidth: 650, padding: 10 }} aria-label="simple table">
           <TableHead sx={{ fontFamily: 'El Messiri', fontSize: 18 }}>
             <TableRow>
@@ -42,32 +161,27 @@ const Report = () => {
                 sx={{ fontFamily: 'El Messiri', fontSize: 18, fontWeight: 700 }}>
                 Дата
               </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontFamily: 'El Messiri', fontSize: 18, fontWeight: 700 }}>
-                Адрес
-              </TableCell>
-              <TableCell
+              {/* <TableCell
                 align="center"
                 sx={{ fontFamily: 'El Messiri', fontSize: 18, fontWeight: 700 }}>
                 Статус
-              </TableCell>
+              </TableCell> */}
             </TableRow>
           </TableHead>
           <TableBody>
             {app
               .sort((a, b) => (a.created > b.created ? 1 : -1))
-              .map((row, index) => (
+              .map((row) => (
                 <TableRow
-                  key={row.orderId}
+                  key={row.reportId}
                   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                   <TableCell
                     className="goods"
                     component="th"
                     scope="row"
                     sx={{ fontFamily: 'El Messiri', fontSize: 16 }}>
-                    <Link style={{ textDecoration: 'underline' }} to={row.orderId}>
-                      Заказ №{index + 1}
+                    <Link style={{ textDecoration: 'underline' }} to={`${row.reportId}`}>
+                      Отчет №{row.reportId} {console.log(row.reportId)}
                     </Link>
                   </TableCell>
                   <TableCell
@@ -76,18 +190,12 @@ const Report = () => {
                     sx={{ fontFamily: 'El Messiri', fontSize: 16 }}>
                     {row.created.substring(0, 10)} {row.created.substring(11, 19)}
                   </TableCell>
-                  <TableCell
+                  {/* <TableCell
                     className="goods"
                     align="center"
                     sx={{ fontFamily: 'El Messiri', fontSize: 16 }}>
-                    {/* {row.address} */}
-                  </TableCell>
-                  <TableCell
-                    className="goods"
-                    align="center"
-                    sx={{ fontFamily: 'El Messiri', fontSize: 16 }}>
-                    {/* {row.status} */}
-                  </TableCell>
+                    {row.result}
+                  </TableCell> */}
                 </TableRow>
               ))}
           </TableBody>
--- src/pages/Report/ReportId.jsx
+++ src/pages/Report/ReportId.jsx
@@ -17,129 +17,43 @@ const ReportId = () => {
   myHeaders.append('Authorization', 'Bearer ' + localStorage.getItem('accessToken'));
   useEffect(() => {
     const asyncFn = async () => {
-      const response = await fetch(`${REACT_APP_API}/order/${id}`, { headers: myHeaders }); //,{mode: 'no-cors'}
+      const response = await fetch(`${REACT_APP_API}/report`, { headers: myHeaders }); //,{mode: 'no-cors'}
       const result = await response.json();
-      //   console.log(result);
-      setApp(result);
+      const arr = result.filter((el) => el.reportId.toString() == id);
+      setApp(arr[0]);
     };
     asyncFn();
-    if (localStorage.getItem('goods') === null) {
-      localStorage.setItem('goods', []);
-    }
   }, []);
-
-  function countsum() {
-    let sum = 0;
-    // if (app.length > 0) {
-    app?.items?.map((row) => (sum += parseFloat(row.pricePerPiece) * parseFloat(row.count)));
-    // console.log(sum);
-    // }
-    return sum;
-  }
-
-  const handledeleteorder = () => {
-    const asyncCansel = () => {
-      fetch(`${REACT_APP_API}/order/${id}`, {
-        method: 'PUT',
-        headers: myHeaders,
-        body: JSON.stringify({
-          method: 'cancel'
-        })
-      });
-    };
-    asyncCansel();
-    navigate('/my-order');
-  };
-
-  const addreport = () => {
-    const asyncReport = () => {
-      fetch(`${REACT_APP_API}/order/${id}`, {
-        method: 'PUT',
-        headers: myHeaders,
-        body: JSON.stringify({
-          method: 'report',
-          data: document.getElementById('report').value
-        })
-      });
-    };
-    asyncReport();
-    navigate('/my-order');
-  };
 
   return (
     <div className="fon">
-      <div className="pole">
-        <div className="title2">Мои заказы - Заказ</div>
+      <div className="pole1">
+        <div className="title2">Мои отчеты - Отчет</div>
         <div className="text2">
           Дата: {app?.created?.substring(0, 10)} {app?.created?.substring(11, 19)}
         </div>
-        <div className="text2">Адрес: {app.address}</div>{' '}
         <div className="text2">
-          Позиции:{' '}
-          {app?.items?.map((el) => (
-            <div>
-              <div className="text2">
-                {el.itemName} - {parseInt(el.count)} шт.
-              </div>
-            </div>
+          {app?.photoUris?.map((el, index) => (
+            <img src={`http://localhost:7777${el}`} key={index} />
           ))}
         </div>
-        <div className="text2">Сумма: {countsum()}</div>
-        <div className="text2">Комментарий: {app.comment}</div>
-        <div className="text2">Статус: {app.status}</div>
-      </div>
-      {app.status === 'Created' && (
-        <Button
-          type="submit"
-          onClick={handledeleteorder}
-          variant="contained"
-          style={{
-            width: '200px',
-            backgroundColor: `#3e131b`,
-            fontFamily: 'El Messiri',
-            fontSize: 16,
-            margin: '30px'
-          }}>
-          Отменить заказ
-        </Button>
-      )}
-      {app.status === 'Done' && (
-        <form onSubmit={addreport} className="flex flex-row gap-4 ml-8 mt-4">
-          <input
-            className="h-10 rounded-md pl-2"
-            id="report"
-            type="report"
-            label="report"
-            placeholder="Оставьте отзыв"
-          />
+
+        <div className="text2">Комментарий: {app.text}</div>
+        <div className="text2">Статус: {app.result}</div>
+        <div className="button-left">
           <Button
             type="submit"
+            onClick={() => navigate('/report')}
             variant="contained"
             style={{
-              bottom: 0,
-              right: 0,
               width: '150px',
               backgroundColor: `#90182E`,
               fontFamily: 'El Messiri',
               fontSize: 16
             }}>
-            Отправить
+            Закрыть
           </Button>
-        </form>
-      )}
-      <div className="button-left">
-        <Button
-          type="submit"
-          onClick={() => navigate('/my-order')}
-          variant="contained"
-          style={{
-            width: '150px',
-            backgroundColor: `#90182E`,
-            fontFamily: 'El Messiri',
-            fontSize: 16
-          }}>
-          Закрыть
-        </Button>
+        </div>
       </div>
     </div>
   );
